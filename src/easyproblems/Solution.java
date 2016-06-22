b9f0f6f82403166bad0f45c6d3f16a098d7be559
Nikhil Arora <nikhilarora01@hotmail.com>
2016-06-22 21:56:59 +0530

    palindrome

---
+++ src/easyproblems/Solution.java
@@ -0,0 +1,14 @@
+package easyproblems;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main (String []args){
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		int i=0;
+		int j=a.length()-1;
+	
+
+}
+}
