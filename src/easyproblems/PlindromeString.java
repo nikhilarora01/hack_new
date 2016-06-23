a748921e8b53053bd71136ac2206b46bd8f60239
Nikhil Arora <nikhilarora01@hotmail.com>
2016-06-23 13:17:29 +0530

    Program with error

--- src/easyproblems/PlindromeString.java
+++ src/easyproblems/PlindromeString.java
@@ -1,5 +1,22 @@
 package easyproblems;
 
+import java.util.Scanner;
+
 public class PlindromeString {
+	public static void main (String[]args){
+		Scanner sc=new Scanner(System.in);
+		String a = sc.next();
+		String temp = null;
+		for (int i=0; i<=a.length()-1;i++)
+		{
+			temp=a;
+		}
+		if (temp==a)
+		{
+			System.out.println("Yes");
+		}else
+			System.out.println("No");
+		
+	}
 
 }
