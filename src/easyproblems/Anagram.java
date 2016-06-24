3b3fcd25bb2e743c0ec67f77843ad04fdc9d656d
Nikhil Arora <nikhilarora01@hotmail.com>
2016-06-24 17:22:09 +0530

    anagrams

---
+++ src/easyproblems/Anagram.java
@@ -0,0 +1,14 @@
+package easyproblems;
+
+import java.util.Scanner;
+
+public class Anagram {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		
+
+	}
+
+}
--- src/easyproblems/Solution.java
+++ src/easyproblems/Solution.java
@@ -1,14 +1,19 @@
 package easyproblems;
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	public static void main (String []args){
-		Scanner sc=new Scanner(System.in);
-		String a=sc.next();
-		int i=0;
-		int j=a.length()-1;
-	
+    public static void main (String[]args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        if (a.length()==b.length() && a.equals(b))
+            return true;
+        System.out.println("Yes");
+        
+        	return false;
+        
+    }
+    
+}
 
-}
-}
