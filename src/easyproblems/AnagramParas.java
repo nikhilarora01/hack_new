4fae4ebe3ce4be1d78f45050ed74abf4faea8cd6
Nikhil Arora <nikhilarora01@hotmail.com>
2016-06-30 17:08:44 +0530

    try this method

---
+++ src/easyproblems/AnagramParas.java
@@ -0,0 +1,17 @@
+package easyproblems;
+
+import java.util.Scanner;
+
+public class AnagramParas {
+	public static void main (String[]args){
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		char[] alphabets=null;
+		alphabets=new char[26];
+		char[] A=a.toCharArray();
+		char[] B=b.toCharArray();
+		
+	}
+
+}
