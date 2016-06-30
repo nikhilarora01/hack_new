17391c7173ff8d0244de1e44f83b09063787027f
Nikhil Arora <nikhilarora01@hotmail.com>
2016-06-30 11:44:26 +0530

    anagram latest

--- src/easyproblems/Solution.java
+++ src/easyproblems/Solution.java
@@ -1,19 +1,15 @@
+
 package easyproblems;
-import java.io.*;
 import java.util.*;
 
-public class Solution {
-    public static boolean main (String[]args){
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        String b=sc.next();
-        if (a.length()==b.length() && a.equals(b))
-            return true;
-        System.out.println("Yes");
-        
-        	return false;
-        
-    }
-    
+public class Solution{
+	public static void main (String[]args){
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		char[]alpha =new char[26];
+		int k=0;
+				
+		
+	}
 }
-
