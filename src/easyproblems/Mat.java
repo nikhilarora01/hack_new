80e719a50f48fd0229f95de16ba01db423d67f19
Nikhil Arora <nikhilarora01@hotmail.com>
2016-07-20 17:04:29 +0530

    matrx

--- src/easyproblems/Mat.java
+++ src/easyproblems/Mat.java
@@ -3,27 +3,28 @@ package easyproblems;
 import java.util.Scanner;
 
 public class Mat {
+	
 	public static void main(String[]args){
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number of rows and columns");
 		int row=sc.nextInt();
 		int col=sc.nextInt();
-        int[][] ints = new int[row][col]; //The 2D array 
+        int[][] mat = new int[row][col];
         System.out.println("Enter" +" "+ row*col+" " +"elements"); 
-        for(int cinput = 0; cinput < ints.length; cinput++) //column 
+        for(int cinput = 0; cinput < mat.length; cinput++) //column 
         { 
-                for(int rinput = 0; rinput < ints.length; rinput++) //row 
+                for(int rinput = 0; rinput < mat.length; rinput++) //row 
                 { 
-                        ints[cinput][rinput] = sc.nextInt(); //This will read the line of integers and fill all 9 positions in the array. 
+                        mat[cinput][rinput] = sc.nextInt(); //This will read the line of integers and fill all 9 positions in the array. 
                 } 
         } 
         
-        for(int column = 0; column < ints.length; column++) 
+        for(int column = 0; column < mat.length; column++) 
         { 
-                for(int arow = 0; arow < ints.length; arow++) 
+                for(int arow = 0; arow < mat.length; arow++) 
                 { 
-                        System.out.print(ints[column][arow] + " "); //Outputs the array in a 3x3 grid. 
+                        System.out.print(mat[column][arow] + " "); //Outputs the array in a 3x3 grid. 
                 } 
                 System.out.println(); 
         }
